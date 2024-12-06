43a23f0cd9a5efb336598062258607cb328f4695
Wilfrieddevcreate <118141280+Wilfrieddevcreate@users.noreply.github.com>
2024-12-06 16:17:34 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ next.config.ts
@@ -0,0 +1,11 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+
+  images: {
+    domains: ['i.pravatar.cc'], // Ajoute 'i.pravatar.cc' dans la liste des domaines autorisés
+  },
+
+};
+
+export default nextConfig;
